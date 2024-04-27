351b98f77b74261392b297c1ab9ddcd7231d30d7
Trevor Swan <tcs94@case.edu>
2024-04-27 14:03:27 -0400

    removed some comments

--- gacp.bat
+++ gacp.bat
@@ -1,10 +1,8 @@
 @echo off
 
-rem Check if any arguments are provided
 if "%~1"=="" (
     set commit_msg=updated code
 ) else (
-    rem Combine all command-line arguments into a single string
     set "commit_msg=%~1"
     shift
     :loop
